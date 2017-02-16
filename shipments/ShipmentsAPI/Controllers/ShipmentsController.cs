946493ff0a7d4297a48a535075a482a150b015ed
Rupesh Tiwari <roopkt@gmail.com>
2017-02-16 17:03:47 -0500

    added startup file and controller

---
+++ shipments/ShipmentsAPI/Controllers/ShipmentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShipmentsAPI.Controllers
+{
+    [Authorize]
+    public class ShipmentsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
